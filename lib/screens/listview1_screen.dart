b1b9229aa216f0e0e1dd7c4696dd1a26a9bf7807
Edu Rivas <erivas17@gmail.com>
2022-09-15 16:06:16 -0400

    routes definition

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -23,7 +23,7 @@ class ListView1Screen extends StatelessWidget {
                     title: Text(game),
                     trailing: const Icon(Icons.arrow_forward_ios_outlined),
                   ))
-              .toList()
+              .toList(),
         ],
       ),
     );
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ListView2Screen extends StatelessWidget {
+  const ListView2Screen({Key? key}) : super(key: key);
+
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview Type 1'),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int index) => ListTile(
+          title: Text(options[index]),
+          trailing: const Icon(Icons.arrow_forward_ios_outlined,
+              color: Colors.indigo),
+          onTap: () {
+            final game = options[index];
+            print(game);
+          },
+        ),
+        itemCount: options.length,
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      ),
+    );
+  }
+}
